b7b497fdeccf18cab5fbcf78389b974b1b80858d
danielcorrea95 <daniel-correa05@hotmail.com>
2023-03-29 22:23:20 -0300

    create user with prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
@@ -39,8 +40,16 @@ export default function Register() {
       setValue('username', String(router.query.username))
     }
   }, [router.query?.username, setValue])
+
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users/', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
